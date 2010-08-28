16b4e90cd3911fb137b9efb713c5c213d62efa05
Breccan <jalnyfen@gmail.com>
2010-08-28 17:59:54 +1200

    Fixed room

--- source/game/engine/models/Room.js
+++ source/game/engine/models/Room.js
@@ -13,27 +13,27 @@ Room = new Class({
     return this.exits;
   },
   hasExit: function(exit) {
-   return typeof this.exits[exit] === 'undefined';
+    return typeof this.exits[exit] === 'undefined';
   },
   set_short: function(short) {
-               this.short = short;
+    this.short = short;
   },
   set_long: function(long) {
-              this.long = long;
+    this.long = long;
   },
-  add_exit(dir, loc) {
+  add_exit: function(dir, loc) {
     this.exits[dir] = loc;
   },
   //add an item view inside the room desc. LPC naming style.
   add_item: function(keyword, desc, aliases) {
-              this.items[keyword] = desc;
-              var that = this;
-              var aliases = new Hash(aliases);
-              aliases.each(function(alias) { that.items[alias] = keyword; });
+    this.items[keyword] = desc;
+    var that = this;
+    var aliases = new Hash(aliases);
+    aliases.each(function(alias) { that.items[alias] = keyword; });
   },
   create: function() {
-          this.set_short("Empty place")
-          this.set_long("This room is broken. Pretend you didn't see it")
-  });
+    this.set_short("Empty place")
+    this.set_long("This room is broken. Pretend you didn't see it")
+  }});
 
-           
+
