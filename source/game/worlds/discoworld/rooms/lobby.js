f17fe3942594bd9594ff21b2baf9126cda897336
Yuffster <msteigerwalt@gmail.com>
2010-08-28 01:15:23 +0000

    Whoops, had some JS errors.

--- source/game/worlds/discoworld/rooms/lobby.js
+++ source/game/worlds/discoworld/rooms/lobby.js
@@ -2,7 +2,7 @@ exports.room = new Class({
 
 	Extends: Room,
 
-	function create() {
+	create: function() {
 		this.set_short("a sparsely decorated lobby");
 		this.set_long("This is a large, unfurnished lobby.  It smells like fresh paint.  There's an elevator to the north and a door in the west wall marked \"Exit\".");
 		this.add_item('elevator', "The elevator looks broken.");
