ff5835ea31cdb852b4747761f673d5bbe698eab0
Breccan <jalnyfen@gmail.com>
2010-08-29 21:34:21 +1200

    Content

--- source/game/worlds/discoworld/rooms/lobby.js
+++ source/game/worlds/discoworld/rooms/lobby.js
@@ -4,9 +4,10 @@ exports.room = new Class({
 
 	create: function() {
 		this.set_short("a sparsely decorated lobby");
-		this.set_long("This is a large, unfurnished lobby.  It smells like fresh paint.  There's an elevator to the north and a door in the west wall marked \"Exit\".");
+		this.set_long("This is a large, unfurnished lobby.  It smells like fresh paint.  There's music coming from the north and a door in the west wall marked \"Exit\".");
 		this.add_item('elevator', "The elevator looks broken.");
 		this.add_exit("west", "stairwell_01");
+    this.add_exit("north", "disco_s");
 	}
 
 });
