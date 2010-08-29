1a41815efe031ab576fda72316f23df7c6f85a57
Breccan <jalnyfen@gmail.com>
2010-08-29 13:33:11 +1200

    Added grooveshark

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require.paths.push('source/game/engine');
 require.paths.push('source/game');
 require.paths.push('./');
 require('./lib/mootools').apply(GLOBAL);
-require('./source/game/engine/engine');
 require('./source/game/main');
 require('./client')
 require('./web_server')
